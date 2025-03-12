16261b5edb4c272cb22b387942e0daabfaa473a1
Krishna <krishnakumar356808@gmail.com>
2025-03-12 09:46:20 +0530

    updating env file and firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@ export const environment = {
     apiKey: "dummy-key",
     authDomain: "my-angular-project-5897.firebaseapp.com",
     projectId: "my-angular-project-5897",
-    storageBucket: "my-angular-project-5897.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef1234567890",
-    measurementId: "G-ABCDEFGHIJ"
+    storageBucket: "my-angular-project-5897.firebasestorage.com",
+    messagingSenderId: "448794340881",
+    appId: "1:448794340881:web:511812b2a69d19fbef4bb0",
+    measurementId: "G-9K8ZQS0M0Z"
   }
 }; 
